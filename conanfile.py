01b83b534fabbef03f199b05b7ca93f2b1ac12a7
Uilian Ries <uilianries@gmail.com>
2018-11-07 23:15:11 -0200

    #522 Bump SQlite version
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
 import os
+from conans import ConanFile, CMake, tools
 
 
 class ConanSqlite3(ConanFile):
     name = "sqlite3"
-    version = "3.21.0"
-    year = "2017"
-    sha1 = "ebe33c20d37a715db95288010c1009cd560f2452"
+    version = "3.25.3"
     description = "Self-contained, serverless, in-process SQL database engine."
     url = "http://github.com/bincrafters/conan-sqlite3"
     homepage = "https://www.sqlite.org"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("conan", "sqlite", "database", "sql", "serverless")
     license = "Public Domain"
     generators = "cmake"
     settings = "os", "compiler", "arch", "build_type"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "FindSQLite3.cmake"]
     options = {"shared": [True, False], "enable_json1": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "enable_json1=False", "fPIC=True"
+    default_options = {"shared": False, "enable_json1": False, "fPIC": True}
+    _source_subfolder = "source_subfolder"
 
     def source(self):
-        base_url = "https://www.sqlite.org/" + self.year
-        archive_name = "sqlite-amalgamation-" + self.version.replace(".","") + "000"
-        archive_ext = "zip"
-        download_url = "{0}/{1}.{2}".format(base_url, archive_name, archive_ext)
-        self.output.info("Attempting download of sources from: " + download_url)
-        tools.get(download_url, sha1=self.sha1)
-        os.rename(archive_name, "sources")
+        sha256 = "2ad5379f3b665b60599492cc8a13ac480ea6d819f91b1ef32ed0e1ad152fafef"
+        download_url = "{}/2018".format(self.homepage)
+        archive_name = "sqlite-amalgamation-3250300"
+        tools.get("{}/{}.zip".format(download_url, archive_name), sha256=sha256)
+        os.rename(archive_name, self._source_subfolder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -37,26 +36,29 @@ class ConanSqlite3(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
 
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["ENABLE_JSON1"] = self.options.enable_json1
-        if self.settings.os != "Windows":
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
         cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy("FindSQLite3.cmake", ".", ".")
-        self.copy("*.h", dst="include", src="sources")
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.pdb", dst="lib", keep_path=False)
-        self.copy(pattern="*.so", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        header = tools.load(os.path.join(self._source_subfolder, "sqlite3.h"))
+        license_content = header[3:header.find("***", 1)]
+        tools.save("LICENSE", license_content)
+
+        self.copy("LICENSE", dst="licenses")
+        self.copy("FindSQLite3.cmake")
+
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ['sqlite3']
+        self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
             self.cpp_info.libs.append("dl")
--- test_package/test_package.cpp
+++
@@ -1,8 +0,0 @@
-#include <sqlite3.h>
-
-int main()
-{
-    auto ver = sqlite3_libversion();
-    return 0;
-}
-
