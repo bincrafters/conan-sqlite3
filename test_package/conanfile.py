31a7757c63e709489512a47a7ccb5e15224989be
FlorentP42 <45787476+FlorentP42@users.noreply.github.com>
2019-01-18 17:08:18 +0100

    Skipped test execution if cross building on mac.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,6 +20,7 @@ class TestPackageConan(ConanFile):
             if self.settings.os == "Windows":
                 self.run(bin_path)
             elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+                if not tools.cross_building(self.settings):
+                    self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
                 self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
