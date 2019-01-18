8017a7f06bbf16ff7d486eb3e10d7653444c8ac7
FlorentP42 <45787476+FlorentP42@users.noreply.github.com>
2019-01-18 17:23:52 +0100

    Skipped test execution if cross building for iOS.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -23,4 +23,5 @@ class TestPackageConan(ConanFile):
                 if not tools.cross_building(self.settings):
                     self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+                if not tools.cross_building(self.settings):
+                    self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
