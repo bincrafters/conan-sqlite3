88e75ab7f3961390084e2b5b63f80a34f0bdc9b1
SSE4 <tomskside@gmail.com>
2019-03-19 23:49:31 +0700

    - fix Android builds

--- conanfile.py
+++ conanfile.py
@@ -83,6 +83,8 @@ class ConanSqlite3(ConanFile):
             cmake.definitions["HAVE_POSIX_FALLOCATE"] = False
         if self.settings.os != "Windows":
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        if self.settings.os == "Android":
+            cmake.definitions["HAVE_POSIX_FALLOCATE"] = False
         cmake.configure()
         return cmake
 
